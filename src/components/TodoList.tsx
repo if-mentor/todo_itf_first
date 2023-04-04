5a21c444e7d6c152d3df052d9fe2ced38e327f8e
sakiyama yosuke <sakiyama.yosuke@itfllc.co.jp>
2023-04-04 16:26:19 +0900

    #4 TodoListコンポーネントのレイアウト編集

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -53,8 +53,8 @@ const TodoList: React.FC = () => {
     }
   ]
   return (
-    <div className='flex justify-center'>
-      <table className='table-fixed my-3'>
+    <div className='max-w-5xl mx-auto py-2 flex justify-between'>
+      <table className='w-full table-auto my-3'>
         <thead className='bg-green-400'>
           <tr>
             <th className='text-left pl-2 py-2'>Task</th>
@@ -68,13 +68,13 @@ const TodoList: React.FC = () => {
         <tbody>
           {posts.map((post: Post) => (
             <tr className='border-b'>
-            <td className='text-left pr-10 py-3'>
+            <td className='text-left py-3'>
               <p className='text-sm'>{post.task}</p>
             </td>
-            <td className='px-3'>
+            <td>
               <p 
                 className={`border border-black outline-none rounded-full
-                  font-bold px-2 py-1 text-center
+                  font-bold py-1 text-center
                   ${post.status === 'NOT STARTED' ? 'text-[3px] bg-green-100'
                   : post.status === 'DOING' ? 'text-xs bg-green-700 text-white'
                   : 'text-xs bg-green-400'}
@@ -83,7 +83,7 @@ const TodoList: React.FC = () => {
                 {post.status}
               </p>
             </td>
-            <td className='px-3'>
+            <td className='text-center'>
               <select 
                 value={post.priority}
                 className='border border-red-400 outline-none rounded-md
@@ -94,13 +94,17 @@ const TodoList: React.FC = () => {
                 <option value='Low'>Low</option>
               </select>
             </td>
-            <td className='text-xs px-3'>{post.created_at}</td>
-            <td className='text-xs px-3'>{post.updated_at}</td>
-            <td className='flex justify-items-center px-5 py-3'>
-              <svg className="h-4 w-4 text-gray-500 mr-3"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"/>
-              </svg>
-              <svg className="h-4 w-4 text-gray-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <polyline points="3 6 5 6 21 6" />  <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" /></svg>
+            <td className='text-xs text-center'>{post.created_at}</td>
+            <td className='text-xs text-center'>{post.updated_at}</td>
+            <td className=' text-center py-3'>
+              <button>
+                <svg className="h-4 w-4 text-gray-500 mr-4"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"/>
+                </svg>
+              </button>
+              <button>
+                <svg className="h-4 w-4 text-gray-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <polyline points="3 6 5 6 21 6" />  <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" /></svg>
+              </button>
             </td>
           </tr> 
           ))}
