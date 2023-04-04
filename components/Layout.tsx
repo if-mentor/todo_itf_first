ef735ba1eb25359d7837704dd445f83ab008e186
{kyyysnr} <{kurounnmo@icloud.com}>
2023-04-04 13:50:59 +0900

    #2 検索欄とヘッダーの作成

---
+++ components/Layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <header className='w-full bg-green-400 text-4xl font-bold'>
+      <div className='max-w-5xl mx-auto p-2'>
+        TODO
+      </div>
+    </header>
+    
+  )
+}
+
+export default Layout
