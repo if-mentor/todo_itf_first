06ee9db18888850efe218b8f7810626e057193d5
Taiyo Maruyama <stickytomatobaby0501@gmail.com>
2023-04-06 15:55:41 +0900

    inputのplaceholderがフォーカス時に消えるように変更

--- src/components/InputDetail.tsx
+++ src/components/InputDetail.tsx
@@ -8,7 +8,7 @@ const InputDetail = () => {
         <input
           type="text"
           placeholder="Text"
-          className="text-5 border border-black w-full h-[208px] rounded-[10px] px-4 py-2 placeholder:text-black placeholder:absolute placeholder:text-2xl"
+          className="text-5 border border-black w-full h-[208px] rounded-[10px] px-4 py-2 placeholder:text-black focus:placeholder:text-white placeholder:absolute placeholder:text-2xl"
         />
       </div>
     </div>
--- src/components/InputTitle.tsx
+++ src/components/InputTitle.tsx
@@ -8,7 +8,7 @@ const InputTitle = () => {
         <input
           type="text"
           placeholder="Text"
-          className="text-5 border border-black w-full h-[71px] rounded-[10px] px-4 py-2 placeholder:text-black placeholder:absolute placeholder:text-2xl"
+          className="text-5 border border-black w-full h-[71px] rounded-[10px] px-4 py-2 placeholder:text-black focus:placeholder:text-white  placeholder:absolute placeholder:text-2xl"
         />
       </div>
     </div>
