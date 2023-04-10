b2323eb530318d78db9e6deafa73fe5fab46a53c
Taiyo Maruyama <stickytomatobaby0501@gmail.com>
2023-04-10 15:32:06 +0900

    #24 add:新規作成画面からfirebaseに保存できるよう機能追加

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,20 +1,14 @@
-import InputDetail from "@/components/NewAndEditPageParts/InputDetail";
-import InputTitle from "@/components/NewAndEditPageParts/InputTitle";
-import NewTodoFooter from "@/components/NewAndEditPageParts/NewTodoFooter";
+import NewTodoPage from "@/components/NewAndEditPageParts/NewTodoPage";
 import TodoTop from "@/components/NewAndEditPageParts/TodoTop";
 import React from "react";
 
 const create = () => {
   return (
     <div>
-      <TodoTop  title="NEW TODO"/>
-      <InputTitle  />
-      <InputDetail />
-      <NewTodoFooter />
+      <TodoTop title="NEW TODO" />
+      <NewTodoPage />
     </div>
   );
 };
-
-
 
 export default create;
