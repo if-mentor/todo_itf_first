302df8c0c28c4ee0baf1052a7f5169030afebb70
sakiyama yosuke <sakiyama.yosuke@itfllc.co.jp>
2023-04-12 10:07:41 +0900

    #25 Warnig解消のため、onChangeの追加、その他修正

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -81,6 +81,7 @@ const TodoList: React.FC = () => {
                       value={todo.priority}
                       className='border border-red-400 outline-none rounded-md
                       text-xs p-1'
+                      onChange={(e) => e.target.value}
                     >
                       <option value='High'>High</option>
                       <option value='Middle'>Middle</option>
@@ -92,11 +93,13 @@ const TodoList: React.FC = () => {
                   <td className=' text-center py-3'>
                     <button>
                       <svg className="h-4 w-4 text-gray-500 mr-4"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"/>
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"/>
                       </svg>
                     </button>
                     <button>
-                      <svg className="h-4 w-4 text-gray-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <polyline points="3 6 5 6 21 6" />  <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" /></svg>
+                      <svg className="h-4 w-4 text-gray-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">
+                        <polyline points="3 6 5 6 21 6" />  <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+                      </svg>
                     </button>
                   </td>
                 </tr> 
