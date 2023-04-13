1c8fde0328dc66e57a0a01d71137f40ceb53caf9
{kyyysnr} <{kurounnmo@icloud.com}>
2023-04-13 14:02:23 +0900

    キャメル型に変更

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -8,7 +8,7 @@ const SearchForm = () => {
             <div className='border border-solid border-black rounded-full p-2 mt-5 bg-slate-300'>
                 <Link href="/create">
                 <svg className="h-4 w-4 text-black"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>
                 </svg>
                 </Link>
             </div>
